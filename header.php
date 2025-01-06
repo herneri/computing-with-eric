19b04bb192166d8094e7771759423967adc0099a
Eric Hernandez <herneri@protonmail.com>
2025-01-06 14:16:29 -0600

    feat: Port header and head

---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+	<meta charset="UTF-8"/>
+	<meta name="author" content="Eric Hernandez"/>
+
+	<link rel="stylesheet" type="text/css" href="static/css/index.css"/>
+	<title> <?php echo($title . " - Computing with Eric"); ?> </title>
+</head>
+	
+<body>
+	<header>
+		<h1 id="website-title"> <a href="/"> Computing with Eric </a> </h1>
+		
+		<div id="navigation">
+				<span class="nav-element"> <a href="software.php"> Software</a> </span>
+				<span class="nav-element"> <a href="hardware-projects.php"> Hardware Projects</a> </span>
+				<span class="nav-element"> <a href="https://www.github.com/herneri"> Github</a> </span>
+				<span class="nav-element"> <a href="my-computing.php"> My Computing</a> </span>
+				<span class="nav-element"> <a href="about-me.php"> About Me</a> </span>
+		</div>
+	</header>
