6275a9fb1b84edb3eefa088e881dc62676c726bd
Eric Hernandez <herneri@protonmail.com>
2025-01-06 22:09:12 -0600

    docs: Add license notice to header

--- header.php
+++ header.php
@@ -1,5 +1,24 @@
 <!DOCTYPE html>
 
+<!--
+    Copyright (C) 2025  Eric Hernandez
+
+    This file is part of Computing with Eric.
+
+    Computing with Eric is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    Computing with Eric is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with Computing with Eric.  If not, see <https://www.gnu.org/licenses/>.
+-->
+
 <html>
 <head>
 	<meta charset="UTF-8"/>
