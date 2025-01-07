d988fb4baba00730e388c31c2dc6d597fc98825b
Eric Hernandez <herneri@protonmail.com>
2025-01-06 22:16:07 -0600

    port: Adapt software page

---
+++ software.php
@@ -0,0 +1,13 @@
+<?php 
+	$title = "Software";
+	require_once("header.php");
+?>
+
+	<link rel="stylesheet" type="text/css" href="static/css/software.css"/>
+	
+	<a href="/lang.php?name=c"><img src="static/images/software-assets/c-logo.png" alt="C logo"/></a>
+	<a href="/lang.php?name=python"><img src="static/images/software-assets/python-logo.png" alt="Python logo"/></a>
+	<a href="/lang.php?name=java"><img src="static/images/software-assets/java-logo.png" alt="Java logo"/></a>
+	<a href="/lang.php?name=shell"><img src="static/images/software-assets/bash-logo.png" alt="Bash logo"/></a>
+</body>
+</html>
