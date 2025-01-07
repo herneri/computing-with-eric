d528e13710ef65e7d03973fea3dfc38678f353cc
Eric Hernandez <herneri@protonmail.com>
2025-01-06 22:14:01 -0600

    refactor: Port index page

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+	$title = "Home";
+	require_once("header.php");
+?>
+
+	<img id="eric-picture" src="static/images/eric.jpg" alt="A picture of the website author" height="520" width="390"/>
+
+	<h2> Free Software Developer </h2>
+	<p>
+		This site hosts my software files with their documentation
+		and an introduction to myself.
+	</p>
+</body>
+</html>
