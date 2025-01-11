781a5ed8aab48048b2689e5670ed1a8381a96189
Eric Hernandez <herneri@protonmail.com>
2025-01-10 23:50:06 -0600

    docs: Prepend license header to get-metadata

--- api/get-metadata.php
+++ api/get-metadata.php
@@ -1,5 +1,26 @@
 <?php
 
+/*
+	get-metadata: API endpoint for program metadata services
+
+	Copyright (C) 2025  Eric Hernandez
+
+	This file is part of Computing with Eric.
+
+	Computing with Eric is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as
+	published by the Free Software Foundation, either version 3 of the
+	License, or (at your option) any later version.
+
+	Computing with Eric is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with Computing with Eric.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 require_once("metadata-utils.php");
 header("Content-Type: application/json");
 	
